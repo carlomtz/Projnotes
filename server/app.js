ee4116884c456168c97afdb6ba216b9c6311df5b
carlomtz <l181130104@gamadero.tecnm.mx>
2022-09-28 19:55:49 -0500

    first commit

---
+++ server/app.js
@@ -0,0 +1,67 @@
+// Bliblioteca de terceros para manejar errores http
+// ES5 
+// var createError = require('http-errors');
+// ES6
+import createError from 'http-errors';
+// El framework express
+//var express = require('express');
+import express from 'express';
+//Bliblioteca del nucleo de node que sirve para administrar rutas
+//var path = require('path');
+import path from 'path';
+//Bliblioteca externa que sirve para administrar las cookies
+//var cookieParser = require('cookie-parser');
+import cookieParser from 'cookie-parser';
+//Bliblioteca que registra en consola solicitudes del cliente
+//var logger = require('morgan');
+import logger  from 'morgan';
+//Definicion de rutas 
+//var indexRouter = require('./routes/index');
+import indexRouter from "./routes/index";
+import usersRouter from "./routes/users";
+//var usersRouter = require('./routes/users');
+//Creando una instancia de express
+const app = express();
+
+// view engine setup
+// Configura el motor de plantillas
+//1.Establecer donde estaran las plantillas
+//(Vistas -> Viwes)
+//app.set("<Nombre de la varieable>",<valor>)
+app.set('views', path.join(__dirname, 'views'));
+//Establesco que motor precardago usare
+app.set('view engine', 'hbs');
+//Establezco Middellwares
+app.use(logger('dev'));
+//Middelware para parsear a json la peticion 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+//Servidor de archivos estaticos
+app.use(express.static(path.join(__dirname,'..' ,'public')));
+//Registro de Rutas
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+// function declaration 
+app.use((req, res, next) =>{
+  next(createError(404));
+});
+
+// error handler
+//app.use(function(err, req, res, next) {
+  app.use((err, req, res, next)=> {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+// Exportando la instancia del server "app"
+// ES5
+//module.exports = app;
+// ES6 
+export default app;
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+//notas importnates
+//el archivo de confguracion debe usar es5
+//importar un administrado de rutas de archivos
+const path =require('path');
+//exportamos un objeto de configuracion 
+//que sera usado por webpack
+module.exports={
+    //1. El arcivo de entrada o indexador (contiene las referencias principales)
+    entry: "/client/index.js",
+    //2. Especificar el archivo de salida
+    output: {
+        //2.1 nos especifica la ruta de salida
+        path: path.resolve(__dirname, "public"),
+        //2.2 nombre del archivo de salida
+    filename: "bundle.js"
+    },
+    devServer:{
+        //3.1Folder de archivos estaticos
+        static:path.join(__dirname,"public"),
+        //puerto del servidor de desarrollo
+        port:8080,
+        //3.3 definiendo el host
+        host:"localhost"
+    }
+}
