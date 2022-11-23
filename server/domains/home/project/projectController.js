4bef676eb5ceac94381f8b1c8f13d966f3397f96
carlomtz <l181130104@gamadero.tecnm.mx>
2022-11-23 08:31:21 -0600

    formulario de proyecto

---
+++ server/domains/home/project/projectController.js
@@ -0,0 +1,27 @@
+// Creando los Actions Methods
+// del controlador Project
+
+// GET "/project"
+// GET "/project/list"
+const list = (req, res) => {
+  // 1. Generando el view-model
+  const viewModel = {};
+  // 2. Madamos a generar la vista con el Template Engine
+  res.render('project/list', viewModel);
+};
+
+// GET "/project/add"
+// GET "/project/create"
+const showAddProjectForm = (req, res) => {
+  const viewModel = {};
+  res.render('project/add', viewModel);
+};
+// POST "/project/add"
+// POST "/project/create"
+const addProject = (req, res) => {
+  // Extrayendo la informacion del formulario
+  res.status(200).send('Ha llegado la peticion');
+};
+
+// Exportando el Controlador
+export default { list, showAddProjectForm, addProject };
